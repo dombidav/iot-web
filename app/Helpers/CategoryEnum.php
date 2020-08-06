3bfeec3be6dd88fca2569cef6f469d10236585e1
Dombi Tibor <dombidav@gmail.com>
2020-08-06 15:21:55 +0200

    fixed not working "other" value in CategoryEnum

--- app/Helpers/CategoryEnum.php
+++ app/Helpers/CategoryEnum.php
@@ -7,22 +7,22 @@ namespace App\Helpers;
 use Spatie\Enum\Enum;
 
 /**
- * @method Static self other()
+ * @method static self other()
  * @method static self thermometer()
  * @method static self rfid_reader()
  * @method static self heater()
- * * @method static self fan()
+ * @method static self fan()
  */
 class CategoryEnum extends Enum
 {
     protected static function values(): array
     {
         return [
-            'other' => 0,
-            'thermometer' => 1,
-            'rfid_reader' => 2,
-            'heater' => 3,
-            'fan' => 4
+            'other' => 1,
+            'thermometer' => 2,
+            'rfid_reader' => 3,
+            'heater' => 4,
+            'fan' => 5,
         ];
     }
 }
