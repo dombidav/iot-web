57a18b50948ff6dae2b7f556c5504f9d17eea5a5
Dombi Tibor <dombidav@gmail.com>
2020-08-06 15:22:13 +0200

    Added IDE helpers to Device model

--- app/Device.php
+++ app/Device.php
@@ -3,10 +3,14 @@
 namespace App;
 
 //use Illuminate\Database\Eloquent\Model;
+use Illuminate\Pagination\LengthAwarePaginator;
 use Jenssegers\Mongodb\Eloquent\Model;
+use Jenssegers\Mongodb\Helpers\EloquentBuilder;
 
 /**
  * @property string name
+ * @method static EloquentBuilder where(string $field, string $operator, mixed $value)
+ * @method static LengthAwarePaginator paginate(int $int)
  */
 class Device extends Model
 {
