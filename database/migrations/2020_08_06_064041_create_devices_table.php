4426afa66e196750bbfef07fd01fc67d97b6aae5
Dombi Tibor <dombidav@gmail.com>
2020-08-06 15:19:54 +0200

    Changed default category to "1" in device table migration

--- database/migrations/2020_08_06_064041_create_devices_table.php
+++ database/migrations/2020_08_06_064041_create_devices_table.php
@@ -15,7 +15,7 @@ class CreateDevicesTable extends Migration
     {
         Schema::create('devices', function (Blueprint $table) {
             $table->string('name');
-            $table->integer('category')->default(0);
+            $table->integer('category')->default(1);
             $table->timestamps();
         });
     }
