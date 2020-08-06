4226f729e96b7f55c6631633b831805c6bfe9364
Dombi Tibor <dombidav@gmail.com>
2020-08-06 10:20:05 +0200

    Generated models

---
+++ database/factories/WorkerGroupFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\WorkerGroup;
+use Faker\Generator as Faker;
+
+$factory->define(WorkerGroup::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
