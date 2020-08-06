b284517a83881c5fc0cd5b23af739d3ffd3d42e5
Dombi Tibor <dombidav@gmail.com>
2020-08-06 10:49:36 +0200

    Added calls to DatabaseSeeders.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+         $this->call(UserSeeder::class);
+        $this->call(DeviceSeeder::class);
+        $this->call(WorkerSeeder::class);
+        $this->call(WorkerGroupSeeder::class);
+        $this->call(LogSeeder::class);
+        $this->call(LockSeeder::class);
     }
 }
