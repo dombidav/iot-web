ba278fb8101fd6e20eccd92392204b78ec14e56d
Dombi Tibor <dombidav@gmail.com>
2020-08-07 09:20:22 +0200

    Changed "category" atribute to return CategoryEnum class in DeviceModel

--- app/Device.php
+++ app/Device.php
@@ -3,17 +3,25 @@
 namespace App;
 
 //use Illuminate\Database\Eloquent\Model;
+use App\Helpers\CategoryEnum;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Jenssegers\Mongodb\Eloquent\Model;
 use Jenssegers\Mongodb\Helpers\EloquentBuilder;
 
 /**
  * @property string name
+ * @property CategoryEnum category
  * @method static EloquentBuilder where(string $field, string $operator, mixed $value)
  * @method static LengthAwarePaginator paginate(int $int)
  */
 class Device extends Model
 {
-    //protected $connection = "mongoDB";
-   // protected $table =
+
+    public function getCategoryAttribute($category){
+        try {
+            return CategoryEnum::fromValue($category);
+        }catch (\BadMethodCallException $exception){
+            return null;
+        }
+    }
 }
