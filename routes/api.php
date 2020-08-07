b1c57778550546db513d7ca46cb06d8300aa584e
Dombi Tibor <dombidav@gmail.com>
2020-08-07 10:27:57 +0200

    Removed unnecessary routes from api.php

--- routes/api.php
+++ routes/api.php
@@ -21,19 +21,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::post('/workerauth','AuthenticateWorkerController@auth')->name("workerauth");
 
-Route::get('/fan/{device}/{user}','FanController@show')->name("fan.show"); // Finds {Device}, authenticates {user}, gets the fan speed
-
-Route::put('/fan/{device}/{user}','FanController@update')->name("fan.update"); // Finds {Device}, authenticates {user}, sets the thermometer value
-
-
-Route::get('/heat/{device}/{user}','HeatingController@show')->name("heat.show"); // Finds {Device}, authenticates {user}, gets the heating status and thermometer data
-
-
-Route::put('/heat/{device}/{user}','HeatingController@update')->name("heat.update"); // Finds {Device}, authenticates {user}, sets the heating status
-
-
-Route::get('/camera/{device}/{user}','CameraController@show')->name("camera.show"); // Finds {Device}, authenticates {user}, gets the camera stream
-
 
 Route::resource('worker','WorkerController');
 Route::resource('workergroup','WorkerGroupController');
