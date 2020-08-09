e913154c848e98cd36570a1e499593b63e480076
Dombi Tibor <dombidav@gmail.com>
2020-08-09 14:18:36 +0200

    added Log connections

--- app/Log.php
+++ app/Log.php
@@ -7,5 +7,15 @@ use Jenssegers\Mongodb\Eloquent\Model;
 
 class Log extends Model
 {
-    //
+    public function user(){
+        return $this->hasOne('App\User');
+    }
+
+    public function worker(){
+        return $this->hasOne('App\Worker');
+    }
+
+    public function devices(){
+        return $this->belongsToMany('App\Device');
+    }
 }
--- app/User.php
+++ app/User.php
@@ -39,4 +39,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function logs(){
+        return $this->belongsToMany('App\Log');
+    }
 }
