f67a90de9348f7da5bf1ca66a962e9e6fd4a069e
Dombi Tibor <dombidav@gmail.com>
2020-08-10 15:16:04 +0200

    Changed ResponseWrapper return type to JSON

--- app/Helpers/ResponseWrapper.php
+++ app/Helpers/ResponseWrapper.php
@@ -6,6 +6,7 @@ namespace App\Helpers;
 
 use Exception;
 use Illuminate\Contracts\Routing\ResponseFactory;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Response;
 
 class ResponseWrapper
@@ -15,7 +16,7 @@ class ResponseWrapper
      * @param string $message
      * @param array $response_fields
      * @param int $status_code
-     * @return ResponseFactory|Response
+     * @return JsonResponse
      */
     public static function wrap(string $message, array $response_fields, int $status_code = 400) {
         $array = [
@@ -23,7 +24,7 @@ class ResponseWrapper
             'request' => $response_fields,
             'code' => $status_code
         ];
-        return response(json_encode($array), 404)->header('Content-Type', 'application/json');
+        return response()->json($array)->setStatusCode($status_code);
     }
 
     /**
@@ -31,7 +32,7 @@ class ResponseWrapper
      * @param Exception $exception
      * @param array $response_fields
      * @param int $status_code
-     * @return ResponseFactory|Response
+     * @return JsonResponse
      */
     public static function wrapError(Exception $exception, array $response_fields, int $status_code = 500){
         return self::wrap($exception->getMessage(), $response_fields, $status_code);
