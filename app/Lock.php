4fb953e0ba203a2f41e9f51cbeaad4236ca2d076
Dombi Tibor <dombidav@gmail.com>
2020-08-10 10:59:40 +0200

    Added IDE helpers to Lock & Worker models

--- app/Lock.php
+++ app/Lock.php
@@ -3,10 +3,14 @@
 namespace App;
 
 //use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Collection;
 use Jenssegers\Mongodb\Eloquent\Model;
 
 /**
  * @property string name
+ * @property Collection groups
+ * @method static Lock findOrFail($get)
+ * @method static find($get)
  */
 class Lock extends Model
 {
--- app/Worker.php
+++ app/Worker.php
@@ -2,16 +2,31 @@
 
 namespace App;
 
-//use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Pagination\LengthAwarePaginator;
 use Jenssegers\Mongodb\Eloquent\Model;
+use Jenssegers\Mongodb\Query\Builder;
 
 /**
- * @method static paginate(int $int)
+ * @method static LengthAwarePaginator paginate(int $int)
+ * @method static Builder where(string $string, $worker_rfid)
+ * @method static Worker firstWhere(string $string, $worker_rfid)
  * @property string name
  * @property string rfid
+ * @property Group[] groups
  */
 class Worker extends Model
 {
+    /**
+     * Shorthand for Worker::firstWhere('RFID', $worker_rfid);
+     * @param $worker_rfid
+     * @return Worker
+     */
+    public static function rfid($worker_rfid)
+    {
+        return Worker::where('RFID', $worker_rfid)->first();
+    }
+
     public function groups(){
         return $this->belongsToMany('App\Group');
     }
