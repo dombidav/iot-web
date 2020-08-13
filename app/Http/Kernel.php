3638ff7d39de645d1d220ccee97f8164d8ccd72e
AlaricBell <alaric.bell@gmail.com>
2020-08-13 11:22:06 +0200

    Fixed key authentication middleware path

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,6 +63,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'key.auth' => \Illuminate\Auth\Middleware\ApiKeyAuthMiddleware::class,
+        'key.auth' => \App\Http\Middleware\ApiKeyAuthMiddleware::class,
     ];
 }
