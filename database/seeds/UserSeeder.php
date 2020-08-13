a57004a06b1669ec126ad1a44506def063221678
Dombi Tibor <dombidav@gmail.com>
2020-08-13 13:36:37 +0200

    Changed supervisor api-key

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -17,7 +17,7 @@ class UserSeeder extends Seeder
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => 'CxRGWo3TBq',
-            'api-key' => '0755f8-7a1044-6f23f8-f5b4db-24371b-06998'
+            'api-key' => '6e3f9b-fd0dd7-3328a3-e51762-93a0d6-81951'
         ]);
         factory(App\User::class)->create([
             'name' => 'Supervisor2',
