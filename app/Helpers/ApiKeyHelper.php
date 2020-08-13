16d38b98615985fd35d5ec810bccfb295c2410f6
Dombi Tibor <dombidav@gmail.com>
2020-08-13 13:27:02 +0200

    Added array check to api-key helper

--- app/Helpers/ApiKeyHelper.php
+++ app/Helpers/ApiKeyHelper.php
@@ -3,9 +3,16 @@
 
 namespace App\Helpers;
 
-
+/**
+ * Class ApiKeyHelper Helper class to generate & validate API-Keys
+ * @package App\Helpers
+ */
 class ApiKeyHelper
 {
+    /**
+     * Generates a new API-Key with checksum
+     * @return string Generated Api-Key with check sum
+     */
     public static function generate(){
         $keyPart = implode('-', str_split(substr(strtolower(md5(microtime().rand(1000, 9999))), 0, 30), 6));
         return $keyPart . '-' . self::getSum($keyPart) ;
@@ -24,7 +31,14 @@ class ApiKeyHelper
         return $sum;
     }
 
+    /**
+     * Checks a string if it's a valid API-Key
+     * @param string $key
+     * @return bool
+     */
     public static function isValid(string $key){
+        if(is_array($key))
+            $key = $key[0];
         if(!preg_match('/^(?:[0-9a-f]{6}-){5}(?:[0-9a-f]{5})$/', $key)) return false;
         $keyPart = substr($key, 0, 34);
         $keySum = substr($key, 35);
