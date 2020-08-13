f3bd71310bb5174b032062df42ad9f3e1fd5a734
Dombi Tibor <dombidav@gmail.com>
2020-08-13 10:32:50 +0200

    Added isAlive attribute to Device & Lock

--- app/Device.php
+++ app/Device.php
@@ -4,15 +4,33 @@ namespace App;
 
 //use Illuminate\Database\Eloquent\Model;
 use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Facades\Date;
 use Jenssegers\Mongodb\Eloquent\Model;
 use Jenssegers\Mongodb\Helpers\EloquentBuilder;
 
 /**
  * @property string name
+ * @property mixed updated_at
+ * @property mixed timeout
  * @method static EloquentBuilder where(string $field, string $operator, mixed $value)
  * @method static LengthAwarePaginator paginate(int $int)
  */
 class Device extends Model
 {
+    protected $guarded = [];
+    protected $appends = ['aliveUntil', 'isAlive'];
 
+    public function getAliveUntilAttribute(){
+        return Date::now()->subSeconds($this->timeout ?? 30);
+    }
+
+    public function getIsAliveAttribute(){
+        return $this->updated_at > $this->getAliveUntilAttribute();
+    }
+
+    public function keepAlive(){
+        $this->setUpdatedAt(Date::now());
+        $this->save();
+        return $this;
+    }
 }
--- app/Lock.php
+++ app/Lock.php
@@ -4,18 +4,37 @@ namespace App;
 
 //use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\Date;
 use Jenssegers\Mongodb\Eloquent\Model;
 
 /**
  * @property string name
  * @property Collection groups
  * @property mixed status
+ * @property mixed updated_at
  * @method static Lock findOrFail($get)
  * @method static Lock find($get)
  */
 class Lock extends Model
 {
+    protected $guarded = [];
+    protected $appends = ['aliveUntil', 'isAlive'];
+
     public function groups(){
         return $this->belongsToMany('App\Group');
+    }
+
+    public function getAliveUntilAttribute(){
+        return Date::now()->subSeconds($this->timeout ?? 30);
+    }
+
+    public function getIsAliveAttribute(){
+        return $this->updated_at > $this->getAliveUntilAttribute();
+    }
+
+    public function keepAlive(){
+        $this->setUpdatedAt(Date::now());
+        $this->save();
+        return $this;
     }
 }
