410ee3928881fb995f789f91eee0bd14de62dcae
Dombi Tibor <dombidav@gmail.com>
2020-08-24 12:52:45 +0200

    removed welcome route from web routes

--- src/laravel/routes/web.php
+++ src/laravel/routes/web.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/login', function (){
     return view('admin.index');
 })->name('login');
