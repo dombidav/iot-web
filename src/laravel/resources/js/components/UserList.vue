4a0a22cc03e5779693be862ba0882cf788a57ea1
Dombi Tibor <dombidav@gmail.com>
2020-08-25 14:17:27 +0200

    Implemented UserList component

--- src/laravel/resources/js/components/UserList.vue
+++ src/laravel/resources/js/components/UserList.vue
@@ -1,48 +1,120 @@
 <template>
     <div>
         <h3>User List</h3>
-        <div class="alert alert-danger" v-if="has_error">
-            <p>Couldn't load user list</p>
-        </div>
-        <table class="table">
-            <tr>
-                <th scope="col">Id</th>
-                <th scope="col">Name</th>
-                <th scope="col">Email</th>
-                <th scope="col">Registration date</th>
+        <data-table
+            id="data-table"
+            :data="data"
+            :columns="columns"
+            @onTablePropsChanged="reloadTable">
+            <div slot="filters" slot-scope="{ tableData, perPage }">
+                <div class="row mb-2">
+                    <div class="col-md-6">
+                        <select class="form-control" v-model="tableProps.length" @change="reloadTable">
+                            <option :key="page" v-for="page in perPage" >{{ page }}</option>
+                        </select>
+                    </div>
+                    <div class="col-md-4">
+                        <input type="text" v-model="tableProps.search" placeholder="Search User" @input="reloadTable"/>
+                    </div>
+                    <div class="col-md-2">
+                        <a href="#" class="btn btn-primary"><i class="fas fa-plus"></i></a>
+                    </div>
+                </div>
+            </div>
+            <tbody slot="body" slot-scope="{ data }">
+            <tr
+                :key="item._id"
+                @click="showRowNumber(item._id)"
+                v-for="item in data">
+                <!-- TODO: Hover Effect -->
+                <td
+                    :key="column.name"
+                    v-for="column in columns">
+                    <data-table-cell
+                        :value="item"
+                        :name="column.name"
+                        :meta="column.meta"
+                        :comp="column.component"
+                        :classes="column.classes">
+                    </data-table-cell>
+                </td>
             </tr>
-            <tr v-for="user in users" v-bind:key="user.id" style="margin-bottom: 5px;">
-                <th scope="row">{{ user.id }}</th>
-                <td>{{ user.name }}</td>
-                <td>{{ user.email }}</td>
-                <td>{{ user.created_at }}</td>
-            </tr>
-        </table>
+            </tbody>
+        </data-table>
     </div>
 </template>
 <script>
+    /**
+     * @link https://github.com/jamesdordoy/laravel-vue-datatable
+     * @link https://jamesdordoy.github.io/laravel-vue-datatable/examples/override-filters
+     */
+    import vSelect from 'vue-select';
+    import 'vue-select/dist/vue-select.css';
+
     export default {
         data() {
             return {
-                has_error: false,
-                users: null
+                url: "/user",
+                search: "",
+                data: {},
+                selectOptions: [],
+                tableProps: {
+                    search: '',
+                    length: 10,
+                    column: '_id',
+                    dir: 'asc'
+                },
+                columns: [
+                    {
+                        label: 'ID',
+                        name: '_id',
+                        columnName: '_id',
+                        orderable: true,
+                    },
+                    {
+                        label: 'Name',
+                        name: 'name',
+                        columnName: 'name',
+                        orderable: true,
+                    },
+                    {
+                        label: 'Email',
+                        name: 'email',
+                        columnName: 'email',
+                        orderable: true,
+                    },
+                ]
             }
         },
-        mounted() {
-            this.getUsers()
+        created() {
+            this.getData();
         },
         methods: {
-            getUsers() {
-                this.$http({
-                    url: `user`,
-                    method: 'GET'
+            getData: function () {
+                console.log(this.tableProps.length)
+                axios.get(this.url, {
+                    params: this.tableProps,
+                    headers: {
+                        'api-key': this.$auth.user().apiKey
+                    }
                 })
-                    .then((res) => {
-                        this.users = res.data.users
-                    }, () => {
-                        this.has_error = true
+                    .then(response => {
+                        this.data = response.data;
                     })
+                    .catch(errors => {
+                        console.log(errors.message)
+                    })
+            },
+            reloadTable() {
+
+                this.getData();
+            },
+            showRowNumber(id) {
+                alert(`you clicked row ${id}`);
             }
+        },
+        components:{
+            vSelect,
         }
     }
 </script>
