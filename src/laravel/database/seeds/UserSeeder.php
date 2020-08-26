f7f4736b43edc763f87c7728849892a400e68846
Dombi Tibor <dombidav@gmail.com>
2020-08-26 13:51:17 +0200

    fixed typo in User Seeder

--- src/laravel/database/seeds/UserSeeder.php
+++ src/laravel/database/seeds/UserSeeder.php
@@ -17,7 +17,7 @@ class UserSeeder extends Seeder
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => 'CxRGWo3TBq',
-            'api-key' => '6e3f9b-fd0dd7-3328a3-e51762-93a0d6-81951'
+            'apiKey' => '6e3f9b-fd0dd7-3328a3-e51762-93a0d6-81951'
         ]);
         factory(App\User::class)->create([
             'name' => 'Supervisor2',
@@ -25,7 +25,7 @@ class UserSeeder extends Seeder
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => 'CxRGWo3TBs',
-            'api-key' => '0e3d8e-2a80bd-caffd4-e45876-1c2b1a-24947'
+            'apiKey' => '0e3d8e-2a80bd-caffd4-e45876-1c2b1a-24947'
         ]);
         factory(App\User::class, 30)->create();
     }
