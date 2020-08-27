de8fbb9bbe003d8fce5445f5022b33dafa6790b8
Dombi Tibor <dombidav@gmail.com>
2020-08-27 08:30:19 +0200

    Created OperationButton component for datatable

---
+++ src/laravel/resources/js/components/OperationButtons.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="btn-group" role="group" aria-label="Operations">
+        <a :href="`${model}/${item._id}/edit`" class="btn btn-primary"><i class="fas fa-edit"/> Edit</a>
+        <a href="#" class="btn btn-danger">Delete <i class="fas fa-trash"/></a>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'OperationButtons',
+        props: {
+            item: {},
+            model: {}
+        }
+    }
+</script>
