928acf880b31ba9235e007f1aa6c84547a720642
Dombi Tibor <dombidav@gmail.com>
2020-09-01 11:46:56 +0200

    Created creation form

--- src/laravel/resources/js/components/form/inputs/CreationForm.vue
+++ src/laravel/resources/js/components/form/inputs/CreationForm.vue
@@ -1,10 +1,21 @@
 <template>
-    $END$
+    <div class="row mb-2">
+        <InputDevice v-if="model === 'device'" />
+        <InputDevice v-if-else="model === 'group'" />
+        <InputDevice v-if-else="model === 'lock'" />
+        <InputDevice v-if-else="model === 'worker'" />
+    </div>
 </template>
 
 <script>
+    import InputDevice from "./InputDevice";
     export default {
-        name: "CreationForm"
+        props:{
+            model: {}
+        },
+        components:{
+            InputDevice
+        }
     }
 </script>
 
